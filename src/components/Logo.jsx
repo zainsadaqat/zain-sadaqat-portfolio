059efda07d4aa5ec9ae640a1d99dd4ff48fe4b7a
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-08-21 23:59:40 +0500

    Create logo component

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
-const Logo = () => {
-  return (
-    <h2 className="text-[48px] text-[#fff] font-['Inter'] font-bold">
-      ZS<span className="text-[#72F00F]">.</span>
-    </h2>
-  );
+const Logo = ({ mobile }) => {
+  if (mobile) {
+    return (
+      <h2 className="text-[24px] text-[#fff] font-['Inter'] font-bold">
+        ZS<span className="text-[#72F00F]">.</span>
+      </h2>
+    );
+  } else {
+    return (
+      <h2 className="text-[48px] text-[#fff] font-['Inter'] font-bold">
+        ZS<span className="text-[#72F00F]">.</span>
+      </h2>
+    );
+  }
 };
 
 export default Logo;
