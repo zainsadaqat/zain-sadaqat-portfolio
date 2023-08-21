9afa720c2a12c897f72394cd6d4ef04952ba606c
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-08-21 23:59:55 +0500

    Create navbar comp

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from './Logo';
+import HamburgerIcon from '../assets/HamburgerIcon.png';
+import CrossIcon from '../assets/CrossIcon.png';
+import MobileMenuItem from './MobileMenuItem';
 
 const Navbar = () => {
+  const [openNavBar, setOpenNavBar] = useState(false);
+  const handleClick = (e) => {
+    console.log('Clicked!');
+    setOpenNavBar(!openNavBar);
+  };
   return (
-    <nav>
-      <Logo />
-    </nav>
+    <>
+      <nav className="flex items-center justify-between p-4">
+        <div>
+          <Logo mobile />
+        </div>
+        {!openNavBar ? (
+          <div onClick={handleClick}>
+            <img src={HamburgerIcon} alt="Hamburger" width={24} height={24} />
+          </div>
+        ) : (
+          <div onClick={handleClick}>
+            <img src={CrossIcon} alt="Cross" width={24} height={24} />
+          </div>
+        )}
+      </nav>
+      {openNavBar ? (
+        <div className="">
+          <ul className="flex flex-col items-center justify-center">
+            <MobileMenuItem text="Home" />
+            <MobileMenuItem text="Services" />
+            <MobileMenuItem text="Portfolio" />
+            <MobileMenuItem text="Testimonials" />
+            <MobileMenuItem text="FAQs" />
+            <MobileMenuItem text="Contact" />
+          </ul>
+        </div>
+      ) : null}
+    </>
   );
 };
 
