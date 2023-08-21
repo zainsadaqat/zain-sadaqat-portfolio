78217fe271d1af3d51e967321ae1f4dce8db0efb
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-08-21 15:00:57 +0500

    Add remaining assets

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logo = () => {
+  return (
+    <h2 className="text-[48px] text-[#fff] font-['Inter'] font-bold">
+      ZS<span className="text-[#72F00F]">.</span>
+    </h2>
+  );
+};
+
+export default Logo;
