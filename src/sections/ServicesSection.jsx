d00efd64584e8e15ca7ae3281d3a6723b4470154
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-08-25 00:07:06 +0500

    Create services section

---
+++ src/sections/ServicesSection.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import SubHeading from '../components/SubHeading';
+import SubTitle from '../components/SubTitle';
+import Service from '../components/Service';
+import WebIcon from '../assets/WebIcon.png';
+import DesignIcon from '../assets/DesignIcon.png';
+import MobileIcon from '../assets/MobileIcon.png';
+import MarketingIcon from '../assets/MarketingIcon.png';
+
+const ServicesSection = () => {
+  return (
+    <section className="min-h-[100vh] bg-lightYellow">
+      <div className="pt-[3em]">
+        <SubHeading text="Services" />
+      </div>
+      <SubTitle text="Here’re the services, I'm providing!" />
+      <div
+        className="py-8 
+      flex justify-center items-center flex-wrap"
+      >
+        <Service icon={WebIcon} text="Web Development" />
+        <Service icon={DesignIcon} text="Web Design" />
+        <Service icon={MobileIcon} text="Mobile App Design" />
+        <Service icon={MarketingIcon} text="Digital Marketing" />
+      </div>
+    </section>
+  );
+};
+
+export default ServicesSection;
