be726ea53ee3ab6cdab3d56a4d6f091eca69f79f
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-08-25 00:07:13 +0500

    Add different sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import React from 'react';
 import './App.css';
 import HeroSection from './sections/HeroSection';
+import ServicesSection from './sections/ServicesSection';
+import PortfolioSection from './sections/PortfolioSection';
 function App() {
   return (
-    <>
+    <React.Fragment>
       <HeroSection />
-    </>
+      <ServicesSection />
+      <PortfolioSection />
+    </React.Fragment>
   );
 }
 
