7f9c06e139c9623f71415c2c74a1cc6c47c6b80e
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-08-25 00:05:53 +0500

    Project description

---
+++ src/components/ProjectDescription.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProjectDescription = ({ text }) => {
+  return (
+    <p className="font-semibold text-darkGreen text-[1rem] font-['Raleway']">
+      {text}
+    </p>
+  );
+};
+
+export default ProjectDescription;
