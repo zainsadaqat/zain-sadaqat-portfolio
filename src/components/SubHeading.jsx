65116e477723301e1153e8f9ca923045678a814f
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-08-25 00:05:14 +0500

    Create sub heading to be used in each section

---
+++ src/components/SubHeading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SubHeading = ({ text }) => {
+  return (
+    <h2 className="text-[2rem] text-center font-extrabold text-darkGreen font-['Raleway']">
+      {text}
+    </h2>
+  );
+};
+
+export default SubHeading;
