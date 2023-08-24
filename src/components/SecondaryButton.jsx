ac6ebeb4daceb6f6fa2418682f3c0a0d321ebbd8
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-08-25 00:06:07 +0500

    Update secondary button

--- src/components/SecondaryButton.jsx
+++ src/components/SecondaryButton.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const SecondaryButton = ({ text, color }) => {
+const SecondaryButton = ({ text, icon, color }) => {
   return (
     <div
-      className={`mt-2 text-center p-2 border-[1px] border-[#D0EA83] rounded`}
+      className={`mt-2 flex items-center justify-center text-center p-2 border-[1px] border-[#D0EA83] rounded`}
     >
+      <img src={icon} alt={text} width={24} height={24} />
       <a className={`ml-2 font-bold text-[1rem] text-${color}`}>{text}</a>
     </div>
   );
