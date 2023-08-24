2b3f1f81ac81c2591b23ad8931b46729ef6b0d47
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-08-25 00:04:43 +0500

    Create wrapper for social media icons

---
+++ src/components/SocialMediaIconWrapper.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SocialMediaIconWrapper = ({ children }) => {
+  return <ul className="my-4 flex items-center">{children}</ul>;
+};
+
+export default SocialMediaIconWrapper;
