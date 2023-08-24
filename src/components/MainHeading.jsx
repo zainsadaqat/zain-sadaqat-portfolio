7df5137cb0a091fc3453ac4cd7ec515e996ae0ee
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-08-25 00:06:23 +0500

    Create main heading component

---
+++ src/components/MainHeading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainHeading = () => {
+  return (
+    <h1 className="text-[2.25rem] font-bold text-lightGreen font-['Raleway']">
+      Hey, <br />
+      I’m Zain Sadaqat, <br />A Full Stack Web Developer.
+    </h1>
+  );
+};
+
+export default MainHeading;
