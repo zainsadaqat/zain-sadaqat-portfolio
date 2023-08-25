c6c24a03902e5715ced3157adf51424de7026a8b
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-08-25 23:46:29 +0500

    Test project card

--- src/sections/PortfolioSection.jsx
+++ src/sections/PortfolioSection.jsx
@@ -5,10 +5,15 @@ import ProjectCard from '../components/ProjectCard';
 
 const PortfolioSection = () => {
   return (
-    <section className="min-h-[100vh] bg-lightYellow pt-8">
+    <section className="min-h-[100vh] bg-lightYellow pt-8 px-4">
       <SubHeading text="Portfolio" />
       <SubTitle text="Click on the cards to see my projects." />
-      <ProjectCard />
+      <div className="mt-8">
+        <ProjectCard />
+      </div>
+      <div className="mt-8">
+        <ProjectCard />
+      </div>
     </section>
   );
 };
