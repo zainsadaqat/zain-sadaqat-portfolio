1b7eeece6c22134315e416ebea2fb3786c4673cd
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-08-26 20:44:50 +0500

    Add sections

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -14,7 +14,7 @@ import SocialMediaIconWrapper from '../components/SocialMediaIconWrapper';
 
 const HeroSection = () => {
   return (
-    <section className="bg-darkGreen min-h-[100vh]">
+    <section id="Home" className="bg-darkGreen min-h-[100vh]">
       <Navbar />
       <div>
         <div id="left-side" className="p-4">
@@ -22,22 +22,42 @@ const HeroSection = () => {
           <SocialMediaIconWrapper>
             <li>
               <a href="https://www.twitter.com/zain_sadaqat">
-                <SocialMediaIcon img={XIcon} alt="Twitter" />
+                <SocialMediaIcon
+                  img={XIcon}
+                  alt="Twitter"
+                  width={32}
+                  height={32}
+                />
               </a>
             </li>
             <li className="ml-8">
               <a href="https://www.instagram.com/zainsadaqat.dev">
-                <SocialMediaIcon img={InstagramIcon} alt="Instagram" />
+                <SocialMediaIcon
+                  img={InstagramIcon}
+                  alt="Instagram"
+                  width={32}
+                  height={32}
+                />
               </a>
             </li>
             <li className="ml-8">
               <a href="https://www.linkedin.com/in/zain-sadaqat/">
-                <SocialMediaIcon img={LinkedInIcon} alt="LinkedIn" />
+                <SocialMediaIcon
+                  img={LinkedInIcon}
+                  alt="LinkedIn"
+                  width={32}
+                  height={32}
+                />
               </a>
             </li>
             <li className="ml-8">
               <a href="https://github.com/zainsadaqat">
-                <SocialMediaIcon img={GitHubIcon} alt="GitHub" />
+                <SocialMediaIcon
+                  img={GitHubIcon}
+                  alt="GitHub"
+                  width={32}
+                  height={32}
+                />
               </a>
             </li>
           </SocialMediaIconWrapper>
