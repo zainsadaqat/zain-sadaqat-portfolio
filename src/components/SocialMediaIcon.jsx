d07745bf50c2fa8190b55d89b44d31ed59f3799b
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-08-26 20:44:32 +0500

    Modify social media icon

--- src/components/SocialMediaIcon.jsx
+++ src/components/SocialMediaIcon.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const SocialMediaIcon = ({ img, alt }) => {
-  return <img src={img} alt={alt} width={40} height={40} />;
+const SocialMediaIcon = ({ img, alt, width, height }) => {
+  return <img src={img} alt={alt} width={width} height={height} />;
 };
 
 export default SocialMediaIcon;
