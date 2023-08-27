b3cdd61fea9ecf827dd7a9b21e32e777d437a2e4
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-08-27 14:20:02 +0500

    Add optional icon

--- src/components/SecondaryButton.jsx
+++ src/components/SecondaryButton.jsx
@@ -5,7 +5,7 @@ const SecondaryButton = ({ text, icon, color }) => {
     <div
       className={`mt-2 flex items-center justify-center text-center p-2 border-[1px] border-[#D0EA83] rounded`}
     >
-      <img src={icon} alt={text} width={24} height={24} />
+      {icon && <img src={icon} alt={text} width={24} height={24} />}
       <a className={`ml-2 font-bold text-[1rem] text-${color}`}>{text}</a>
     </div>
   );
