169cb1636397d4c7c798d5d415e554980293d309
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-08-28 23:32:20 +0500

    Modify primary button

--- src/components/PrimaryButton.jsx
+++ src/components/PrimaryButton.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
-const PrimaryButton = ({ text, icon, background, color }) => {
+const PrimaryButton = ({ text, icon }) => {
+  const openWhatsApp = () => {
+    window.open('https://wa.me/+923151467374', '_blank');
+  };
   return (
     <div
-      className={`flex items-center justify-center p-2 rounded bg-[#D0EA83]`}
+      className={`w-full md:w-[200px] flex items-center justify-center p-2 rounded bg-[#D0EA83]`}
+      onClick={openWhatsApp}
     >
       <img src={icon} alt={text} width={32} height={32} />
-      <a className={`ml-2 font-bold text-[1rem] text-${color}`}>{text}</a>
+      <a className={`ml-2 font-bold text-[1rem] text-darkGreen`}>{text}</a>
     </div>
   );
 };
