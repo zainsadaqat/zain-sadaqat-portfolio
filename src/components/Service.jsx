debb5ee6cd17ad3af755b698069da3fa626b6568
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-08-29 21:04:59 +0500

    Add animation

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -4,15 +4,16 @@ const Service = ({ icon, text }) => {
   return (
     <React.Fragment>
       <p
+      id='service-box'
         className="flex items-center justify-center 
-        w-full bg-[#fff] max-w-[600px] min-h-[30vh] 
-        rounded
+        w-full bg-[#fff] max-w-[500px] min-h-[20vh] 
+        border-2 rounded
         font-semibold
         my-2 mx-4
         "
       >
         <img src={icon} alt={text} width={16} height={16} />
-        <span className="ml-[0.5em]">{text}</span>
+        <span className="ml-[0.75em]">{text}</span>
       </p>
     </React.Fragment>
   );
