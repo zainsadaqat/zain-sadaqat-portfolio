4f04004fde1a2d9dcdbd3111b6123f317a9beb4d
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-08-29 21:02:39 +0500

    Modify tailwind conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,25 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     colors: {
-      'darkGreen': '#1F3029',
-      'lightGreen': '#D0EA83',
-      'lightYellow': '#FFF3D0',
+      darkGreen: '#1F3029',
+      lightGreen: '#D0EA83',
+      lightYellow: '#FFF3D0',
+    },
+    screens: {
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      md: '950px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
     },
     extend: {},
   },
