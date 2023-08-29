f6162015390d10ce5f0f6bb19c625a563dc74555
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-08-29 21:04:39 +0500

    Modify secondary button

--- src/components/SecondaryButton.jsx
+++ src/components/SecondaryButton.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const SecondaryButton = ({ text, icon }) => {
   return (
     <div
-      className={`w-full flex items-center justify-center text-center p-2 border-[1px] border-lightGreen rounded mt-2 md:mx-4 md:my-0 md:w-[200px] `}
+      className={`w-full flex items-center justify-center text-center p-4 border-[1px] border-lightGreen rounded mt-2 md:mx-4 md:my-0 md:w-[200px] `}
     >
-      {icon && <img src={icon} alt={text} width={32} height={32} />}
+      {icon && <img src={icon} alt={text} width={24} height={24} />}
       <a
         href="#Services"
         className={`ml-2 font-bold text-[1rem] text-lightGreen`}
