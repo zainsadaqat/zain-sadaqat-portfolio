ca62007bcb9233fbd151b819e27ac1c3bbbe59ea
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-09-04 01:21:04 +0500

    Fix project image

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -16,7 +16,7 @@ const ProjectCard = ({ name, desc, img, liveLink, sourceCode, reverse }) => {
           reverse ? 'md:flex-row-reverse' : 'md:flex-row'
         } md:justify-evenly`}
       >
-        <div id="left-side" className="">
+        <div id="left-side" className="m-4">
           <ProjectTitle text={name} />
           <div className="mt-4">
             <ProjectDescription text={desc} />
@@ -26,11 +26,14 @@ const ProjectCard = ({ name, desc, img, liveLink, sourceCode, reverse }) => {
             <DarkSecondaryButton text="Source Code" icon={SourceCodeIcon} />
           </div>
         </div>
-        <div id="right-side" className="max-w-[100%] my-8 mx-auto">
+        <div
+          id="right-side"
+          className="max-w-[100%] max-h-[100%] m-8 md:m-0 border-2"
+        >
           <img
             src={img}
             alt="SpaceX Adventures Project"
-            className="w-full object-contain"
+            className="w-[500px] h-[300px] object-cover"
           />
         </div>
       </div>
