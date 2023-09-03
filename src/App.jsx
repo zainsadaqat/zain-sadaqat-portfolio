62430df2c5b1df849e28f41d0802157d13e04eee
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-09-03 13:44:16 +0500

    Update GA

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,10 @@ import TestimonialsSection from './sections/TestimonialsSection';
 import FAQSection from './sections/FAQSection';
 import Footer from './sections/Footer';
 import AppointmentSection from './sections/AppointmentSection';
-import ReactGA4 from 'react-ga4';
 
 function App() {
   return (
     <React.Fragment>
-      ReactGA4.initialize('G-N7M2FJYQEJ');
       <HeroSection />
       <ServicesSection />
       <PortfolioSection />
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import ReactGA4 from 'react-ga4';
+ReactGA4.initialize('G-N7M2FJYQEJ');
 
 const handleClick = () => {
   window.scrollTo({
