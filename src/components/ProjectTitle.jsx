666a2e8319f8fb68b0395f2e027d63a333277a91
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-09-04 01:21:15 +0500

    Increase project title size

--- src/components/ProjectTitle.jsx
+++ src/components/ProjectTitle.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ProjectTitle = ({ text }) => {
   return (
-    <h2 className="text-[1.75rem] text-darkGreen font-semibold font-['Work Sans']">
+    <h2 className="text-[2rem] text-darkGreen font-semibold font-['Work Sans'] md:text-[3rem]">
       {text}
     </h2>
   );
