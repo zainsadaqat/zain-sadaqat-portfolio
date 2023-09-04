d6e8227041138fc7d4e0c2c9023f454a1d8c52e1
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-09-04 16:25:43 +0500

    Add about column in the footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -11,9 +11,9 @@ const Footer = () => {
   return (
     <footer id="Contact" className="min-h-[40vh] bg-[#222]">
       <div className="max-w-[1200px] mx-auto">
-        <div className="flex justify-around">
+        <div className="flex flex-col justify-around flex-wrap md:flex-row">
           {/* Col 1 */}
-          <div className="my-8">
+          <div className="my-8 mx-4">
             <Logo />
             <SocialMediaIconWrapper>
               <li>
@@ -59,7 +59,7 @@ const Footer = () => {
             </SocialMediaIconWrapper>
           </div>
           {/* Col 2 */}
-          <div className="my-8">
+          <div className="my-8 mx-4">
             <h3 className="text-[#fff] font-semibold">Resources</h3>
             <ul className="my-2 text-[#bfbfbf]">
               <li className="my-2 hover:underline">
@@ -83,13 +83,13 @@ const Footer = () => {
             </ul>
           </div>
           {/* Col 3 */}
-          <div className="my-8">
-            <h3 className="text-[#fff] font-semibold">Contact Us</h3>
+          <div className="my-8 mx-4">
+            <h3 className="text-[#fff] font-semibold">Contact Me</h3>
             <ul className="my-2 text-[#bfbfbf]">
               <li className="my-2">
                 <a
                   href="mailto:contact@zainsadaqat.com"
-                  className="text-[#ccc] block"
+                  className="text-[#bfbfbf] block"
                 >
                   <span className="hover:underline">
                     contact@zainsadaqat.com
@@ -99,7 +99,7 @@ const Footer = () => {
               <li className="my-2">
                 <a
                   href="mailto:zain.sadaqet@gmail.com"
-                  className="text-[#ccc] block"
+                  className="text-[#bfbfbf] block"
                 >
                   <span className="hover:underline">
                     zain.sadaqet@gmail.com
@@ -107,11 +107,19 @@ const Footer = () => {
                 </a>
               </li>
               <li className="my-2">
-                <a href="tel:+923151467374" className="text-[#ccc] block">
+                <a href="tel:+923151467374" className="text-[#bfbfbf] block">
                   <span className="hover:underline">+92 315 146 7374</span>
                 </a>
               </li>
             </ul>
+          </div>
+          {/* Col 4 */}
+          <div className="my-8 mx-4">
+            <h3 className="text-[#fff] font-semibold">About Me</h3>
+            <p className="max-w-[250px] my-2 text-[1rem] text-[#bfbfbf]">
+              Full Stack Web Developer, UI/UX Designer and Digital Marketer with
+              a Bachelor's in Computer Science.
+            </p>
           </div>
         </div>
         <div className="text-[#fff] text-center py-8">
