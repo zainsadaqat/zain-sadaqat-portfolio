0b95a0714b863af84eb7198a9f76fe94dc628a97
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-09-04 16:01:54 +0500

    Update footer section

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -9,78 +9,118 @@ import GitHubIcon from '../assets/GitHubIcon.png';
 
 const Footer = () => {
   return (
-    <footer id="Contact" className="relative min-h-[50vh] bg-[#222] pt-8 px-4">
+    <footer id="Contact" className="min-h-[40vh] bg-[#222]">
       <div className="max-w-[1200px] mx-auto">
-        <div className="flex flex-col items-center justify-between md:flex-row">
-          <div>
+        <div className="flex justify-around">
+          {/* Col 1 */}
+          <div className="my-8">
             <Logo />
+            <SocialMediaIconWrapper>
+              <li>
+                <a href="https://www.twitter.com/zain_sadaqat">
+                  <SocialMediaIcon
+                    img={XIcon}
+                    alt="Twitter"
+                    width={24}
+                    height={24}
+                  />
+                </a>
+              </li>
+              <li className="ml-4">
+                <a href="https://www.instagram.com/zainsadaqat.dev">
+                  <SocialMediaIcon
+                    img={InstagramIcon}
+                    alt="Instagram"
+                    width={24}
+                    height={24}
+                  />
+                </a>
+              </li>
+              <li className="ml-4">
+                <a href="https://www.linkedin.com/in/zain-sadaqat/">
+                  <SocialMediaIcon
+                    img={LinkedInIcon}
+                    alt="LinkedIn"
+                    width={24}
+                    height={24}
+                  />
+                </a>
+              </li>
+              <li className="ml-4">
+                <a href="https://github.com/zainsadaqat">
+                  <SocialMediaIcon
+                    img={GitHubIcon}
+                    alt="GitHub"
+                    width={24}
+                    height={24}
+                  />
+                </a>
+              </li>
+            </SocialMediaIconWrapper>
           </div>
-          <div className="my-4 text-center md:text-left text-[0.75rem]">
-            <a
-              href="mailto:contact@zainsadaqat.com"
-              className="text-[#fff] block"
-            >
-              <span className="text-[#878787]">Mail to: </span>
-              <span className="hover:underline">contact@zainsadaqat.com</span>
-            </a>
-            <br />
-            <a href="tel:+923151467374" className="text-[#fff] block">
-              <span className="text-[#878787]">Phone # </span>
-              <span className="hover:underline">+92 315 146 7374</span>
-            </a>
+          {/* Col 2 */}
+          <div className="my-8">
+            <h3 className="text-[#fff] font-semibold">Resources</h3>
+            <ul className="my-2 text-[#bfbfbf]">
+              <li className="my-2 hover:underline">
+                <a href="#Home">Home</a>
+              </li>
+              <li className="my-2 hover:underline">
+                <a href="#Services">Services</a>
+              </li>
+              <li className="my-2 hover:underline">
+                <a href="#Portfolio">Portfolio</a>
+              </li>
+              <li className="my-2 hover:underline">
+                <a href="#Testimonials">Testimonials</a>
+              </li>
+              <li className="my-2 hover:underline">
+                <a href="#Schedule a Meeting">Schedule a Meeting</a>
+              </li>
+              <li className="my-2 hover:underline">
+                <a href="#Contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+          {/* Col 3 */}
+          <div className="my-8">
+            <h3 className="text-[#fff] font-semibold">Contact Us</h3>
+            <ul className="my-2 text-[#bfbfbf]">
+              <li className="my-2">
+                <a
+                  href="mailto:contact@zainsadaqat.com"
+                  className="text-[#ccc] block"
+                >
+                  <span className="hover:underline">
+                    contact@zainsadaqat.com
+                  </span>
+                </a>
+              </li>
+              <li className="my-2">
+                <a
+                  href="mailto:zain.sadaqet@gmail.com"
+                  className="text-[#ccc] block"
+                >
+                  <span className="hover:underline">
+                    zain.sadaqet@gmail.com
+                  </span>
+                </a>
+              </li>
+              <li className="my-2">
+                <a href="tel:+923151467374" className="text-[#ccc] block">
+                  <span className="hover:underline">+92 315 146 7374</span>
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
-        <div className="my-8">
-          <SocialMediaIconWrapper>
-            <li>
-              <a href="https://www.twitter.com/zain_sadaqat">
-                <SocialMediaIcon
-                  img={XIcon}
-                  alt="Twitter"
-                  width={24}
-                  height={24}
-                />
-              </a>
-            </li>
-            <li className="ml-8">
-              <a href="https://www.instagram.com/zainsadaqat.dev">
-                <SocialMediaIcon
-                  img={InstagramIcon}
-                  alt="Instagram"
-                  width={24}
-                  height={24}
-                />
-              </a>
-            </li>
-            <li className="ml-8">
-              <a href="https://www.linkedin.com/in/zain-sadaqat/">
-                <SocialMediaIcon
-                  img={LinkedInIcon}
-                  alt="LinkedIn"
-                  width={24}
-                  height={24}
-                />
-              </a>
-            </li>
-            <li className="ml-8">
-              <a href="https://github.com/zainsadaqat">
-                <SocialMediaIcon
-                  img={GitHubIcon}
-                  alt="GitHub"
-                  width={24}
-                  height={24}
-                />
-              </a>
-            </li>
-          </SocialMediaIconWrapper>
-        </div>
-        <div className="text-[#fff] text-center md:text-left py-8">
+        <div className="text-[#fff] text-center py-8">
           <p className="text-[0.75rem]">
             Offering my services to the 🌏 with ❤️ from 🇵🇰
           </p>
           <p className="text-[0.75rem] mt-4">
             &copy;
-            {new Date().getFullYear()} Zain Sadaqat!
+            {new Date().getFullYear()} Zain Sadaqat, All rights are reserved!
           </p>
         </div>
       </div>
