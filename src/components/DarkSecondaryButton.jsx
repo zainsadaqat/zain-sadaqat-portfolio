e9b4d0927e5039cfbef8930128b7cb3ef7d19f44
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-09-05 21:54:11 +0500

    Make project source code button responsive

--- src/components/DarkSecondaryButton.jsx
+++ src/components/DarkSecondaryButton.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const DarkSecondaryButton = ({ text, icon }) => {
   return (
     <div
-      className={`w-full md:w-[200px] mt-2 md:mt-0 flex items-center justify-center text-center p-4 border-[1px] border-darkGreen rounded md:mx-4`}
+      className={`w-full md:w-[220px] mt-2 md:mt-0 flex items-center justify-center text-center p-4 border-[1px] border-darkGreen rounded md:mx-4`}
     >
       <img src={icon} alt={text} width={24} height={24} />
       <a
