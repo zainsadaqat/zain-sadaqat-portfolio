7de4966e44bca65c9c696cb239d9b389718c96a4
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-09-05 21:54:29 +0500

    Remove animation from the picture

--- src/components/MyPicture.jsx
+++ src/components/MyPicture.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './MyPicture.css';
 
 const MyPicture = ({ img, alt }) => {
-  return <img src={img} alt={alt} className="w-full h-full animate-rotate" />;
+  return <img src={img} alt={alt} className="w-full h-full" />;
 };
 
 export default MyPicture;
